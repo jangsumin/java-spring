7663c480dd09e5dc2f120535fca002fbce5f788b
jangsumin <jsm0726@naver.com>
2025-01-16 20:23:17 +0900

    feat: 페이지 이동을 위한 링크와 리다이렉트 기능 사용

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -27,18 +27,15 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         log.info(form.toString());
-        // System.out.println(form.toString());
 
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
         log.info(article.toString());
-        // System.out.println(article.toString());
 
         // 2. 레포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
-        // System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles")
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,14 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Entity // JPA에서 제공하는 어노테이션
+@Getter
+@Setter
 public class Article {
     @Id
     @GeneratedValue
