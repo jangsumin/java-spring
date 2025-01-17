df9568fbda5269d1fa9cd71c8b5f913393b95e5d
jangsumin <jsm0726@naver.com>
2025-01-17 17:13:48 +0900

    feat: 데이터 수정 기능 구현

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -62,4 +62,38 @@ public class ArticleController {
         // 3. 뷰 페이지 반환하기
         return "articles/show";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // id로 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 설정하기
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        // 1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2. 기존 데이터 값을 갱신하기
+        // 클릭을 통해 수정 요청을 하므로 target이 null이 될 일은 없지만, 비정상적인 요청을 막기 위해 조건문을 사용
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 상세 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
