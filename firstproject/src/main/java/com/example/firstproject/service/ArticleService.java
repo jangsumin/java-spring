bd030c38d3d78474998d16f6f65342ad60eef93f
jangsumin <jsm0726@naver.com>
2025-01-28 20:45:24 +0900

    fix: id가 포함된 dto가 생성 요청되었을 때 null을 반환하도록 수정

--- firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -29,6 +29,9 @@ public class ArticleService {
 
     public Article create(ArticleForm dto) {
         Article article = dto.toEntity();
+        if (article.getId() != null) {
+            return null;
+        }
         return articleRepository.save(article);
     }
 
